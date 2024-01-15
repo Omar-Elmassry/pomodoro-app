e669dca7c318407f39f55c184332cec1a263590d
Omar Elmasry <74859926+Omar-Elmassry@users.noreply.github.com>
2024-01-15 18:42:18 +0200

    feat: Add PWA support to the Pomodoro App

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/pomodoro-app/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Pomodoro App",
+        short_name: "Pomodoro App",
+        description: "A simple Pomodoro App",
+        theme_color: "#1e213e",
+        background_color: "#1e213e",
+        display: "standalone",
+        start_url: "/",
+        scope: "/pomodoro-app/",
+        icons: [
+          {
+            src: "manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
